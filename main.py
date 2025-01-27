a089ce278dad611a3187e5247362c3ee50b1381d
JLBarclay <JLBarclayNZ@gmail.com>
2025-01-27 23:54:40 +0800

    changed constants, main, player and new files asteroid and asteroid field

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 
 def main():
@@ -7,16 +9,36 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gameClock = pygame.time.Clock()
     dt = 0
+
+    groupUpdate = pygame.sprite.Group()
+    groupDraw = pygame.sprite.Group()
+    groupAsteroids = pygame.sprite.Group()
+
+    Player.containers = (groupUpdate, groupDraw)
+    Asteroid.containers = (groupAsteroids, groupUpdate, groupDraw)
+    AsteroidField.containers = (groupUpdate)
+
+
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroidField = AsteroidField()
+ 
+
 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
+        #Fill screen background with black
         screen.fill("Black")
-        player.draw(screen)
+
+
+        for object in groupUpdate:
+            object.update(dt)
+
+        for object in groupDraw:
+            object.draw(screen)
 
         #Refresh screen
         pygame.display.flip()
