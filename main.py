5247dfcd6a2c4a18e50f51825be5b7b645870968
JLBarclay <JLBarclayNZ@gmail.com>
2025-01-27 22:59:14 +0800

    Added circleshape class and player class and initialized player on game screen

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameClock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
@@ -11,7 +16,13 @@ def main():
                 return
     
         screen.fill("Black")
+        player.draw(screen)
+
+        #Refresh screen
         pygame.display.flip()
+
+        #Pause refresh for 1/60th of a second and return delta time in seconds
+        dt = gameClock.tick(60) / 1000
 
 
 if __name__ == "__main__":
